35b4b125eb1b8ce3ce1405d2d0b6a3fb259e579a
Mukhammadyusuf Abdurakhimov <Mukhammadyusuf_Abdurakhimov@epam.com>
2021-09-20 18:48:00 +0500

    Configuring dotenv

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const express = reqire("express");
+require("dotenv").config();
+const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+$_PORT = process.env.PORT || 3000;
 app.listen(
   $_PORT,
   console.log(`Application has been started on the port: ${$_PORT}`)
